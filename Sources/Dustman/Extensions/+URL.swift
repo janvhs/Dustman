c82114177dedc9143f77c1689164ab53765bf0f7
Jan Fooken <git@bode.fun>
2023-03-28 11:30:08 +0200

    ✨ Refactor availability check in absolutePath

--- Sources/Dustman/Extensions/+URL.swift
+++ Sources/Dustman/Extensions/+URL.swift
@@ -5,14 +5,15 @@ extension URL {
         let filePath: String
 
         // TODO: Find the support matrix for this.
-        #if os(macOS)
+        #if os(Linux)
+            filePath = absoluteURL.path
+        #else
+            // TODO: Check if this can be removed because of the minimum os version set in Package.swift.
             if #available(OSX 13.0, *) {
                 filePath = absoluteURL.path()
             } else {
                 filePath = absoluteURL.path
             }
-        #else
-            filePath = absoluteURL.path
         #endif
 
         return filePath
