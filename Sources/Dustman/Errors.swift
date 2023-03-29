a7e5624ffe87aded13d9270ba84ff3969af5d71d
Jan Fooken <git@bode.fun>
2023-03-29 12:00:17 +0200

    🩹 Resolve a bunch of todos

--- Sources/Dustman/Errors.swift
+++ Sources/Dustman/Errors.swift
@@ -1,9 +1,25 @@
+import DustbinCore
 import Foundation
 
+// TODO: Move to LocalizedError.
+
+/// Application-specific errors.
 enum DustmanError: Error {}
 
 extension DustmanError: CustomStringConvertible {
     var description: String {
         switch self {}
+    }
+}
+
+extension DustbinError: CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case .setUIDFailed:
+            return "Failed to ensure the user's UID."
+        case let .noSuchFileOrDirectory(fileUrl):
+            let filePathAsString = fileUrl.canonicalizePath()
+            return "No such file or directory: \(filePathAsString)"
+        }
     }
 }
